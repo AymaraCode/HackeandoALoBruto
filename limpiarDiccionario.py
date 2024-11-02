bbe9d06549a548095bd820a9d3780edced75cf0d
Aymara Aguayo <aguayoaymara@gmail.com>
2024-11-02 03:24:52 +0100

    Primer commit, Aymara, Programa para modificacion de diccionario

---
+++ limpiarDiccionario.py
@@ -0,0 +1,29 @@
+
+dicNormal = open("Diccionario.txt", 'r', encoding='latin-1')
+dicNormal.readline()
+dicNormal.readline()
+dicLimpio = open("DiccionarioLimpio.txt", 'w')
+
+while (palabra := dicNormal.readline()) != "":
+    if len(list(palabra))> 3:
+        palabraNueva = ""
+        for letra in palabra.upper():
+            if letra == "Á":
+                palabraNueva += "A"
+            elif letra == "É":
+                palabraNueva += "E"
+            elif letra == "Í":
+                palabraNueva += "I"
+            elif letra == "Ó":
+                palabraNueva += "O"
+            elif letra == "Ú":
+                palabraNueva += "U"
+            elif letra =="Ñ":
+                palabraNueva += "NN"
+            elif letra == "Ü":
+                palabraNueva += "U"
+            else:
+                palabraNueva += letra
+        dicLimpio.write(palabraNueva)
+dicLimpio.close()
+dicNormal.close()
