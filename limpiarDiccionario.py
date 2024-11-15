a193cb46287df92efa64b1ab27f61ff68c1332eb
Aymara Aguayo <aguayoaymara@gmail.com>
2024-11-16 00:25:57 +0100

    commit final final

--- limpiarDiccionario.py
+++ limpiarDiccionario.py
@@ -1,4 +1,5 @@
-
+#Compia las palabras de un diccionario a un nuevo archivo "DiccionarioLimpio.txt"
+#Cambia las letras que dan problemas como las vocales con acento y dieresis y las eñes
 dicNormal = open("Diccionario.txt", 'r', encoding='latin-1')
 dicNormal.readline()
 dicNormal.readline()
