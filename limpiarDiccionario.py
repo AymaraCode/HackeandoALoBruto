c1ead45b0257cd6e7fca42b4b53b80126ed1193a
Aymara Aguayo <aguayoaymara@gmail.com>
2024-11-16 00:31:24 +0100

    commit  final final final

--- limpiarDiccionario.py
+++ limpiarDiccionario.py
@@ -1,30 +1,27 @@
 #Compia las palabras de un diccionario a un nuevo archivo "DiccionarioLimpio.txt"
 #Cambia las letras que dan problemas como las vocales con acento y dieresis y las eñes
 dicNormal = open("Diccionario.txt", 'r', encoding='latin-1')
-dicNormal.readline()
-dicNormal.readline()
 dicLimpio = open("DiccionarioLimpio.txt", 'w')
 
 while (palabra := dicNormal.readline()) != "":
-    if len(list(palabra))> 3:
-        palabraNueva = ""
-        for letra in palabra.upper():
-            if letra == "Á":
-                palabraNueva += "A"
-            elif letra == "É":
-                palabraNueva += "E"
-            elif letra == "Í":
-                palabraNueva += "I"
-            elif letra == "Ó":
-                palabraNueva += "O"
-            elif letra == "Ú":
-                palabraNueva += "U"
-            elif letra =="Ñ":
-                palabraNueva += "NN"
-            elif letra == "Ü":
-                palabraNueva += "U"
-            else:
-                palabraNueva += letra
-        dicLimpio.write(palabraNueva)
+    palabraNueva = ""
+    for letra in palabra.upper():
+        if letra == "Á":
+            palabraNueva += "A"
+        elif letra == "É":
+            palabraNueva += "E"
+        elif letra == "Í":
+            palabraNueva += "I"
+        elif letra == "Ó":
+            palabraNueva += "O"
+        elif letra == "Ú":
+            palabraNueva += "U"
+        elif letra =="Ñ":
+            palabraNueva += "NN"
+        elif letra == "Ü":
+            palabraNueva += "U"
+        else:
+            palabraNueva += letra
+    dicLimpio.write(palabraNueva)
 dicLimpio.close()
 dicNormal.close()
